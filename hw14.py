a0e5c10c484cefce31a2db3ff27b16449d97a27e
JohnTenniel <johntenniel@outlook.com>
2024-03-23 11:14:50 +0700

    new

--- hw14.py
+++ hw14.py
@@ -1,3 +1,5 @@
+# вывод чисел будет в float формате, т.к. неизвестно какие цифры могут быть использованы,
+# как разделить на int и float пока не смог придумать.
 count = 0
 
 
@@ -8,19 +10,28 @@ def arg_deco(func):
         print('Сумма чисел:', result)
         print('Среднее арифметическое: ', end="")
         return sr
+
     return warp
 
 
 @arg_deco
-def summ(*args):
+def summ():
     n = 0
-    print('Вводите цифры пока не введете 0:')
-    while args != 0:
-        args = int(input('->'))
-        n += args
-        if args != 0:
-            global count
-            count += 1
+    print('Вводите цифры, чтобы остановить введите пустую строку:')
+    while True:
+        args = input('->')
+        if not args:
+            break
+        else:
+            try:
+                args = float(args)
+            except:
+                print("only num")
+                continue
+            n += args
+            if args != 0:
+                global count
+                count += 1
 
     return n
 
