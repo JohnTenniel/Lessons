a8f43dde0fb97192d035b11ada2aa68deb2faa9d
JohnTenniel <johntenniel@outlook.com>
2024-04-14 10:33:08 +0700

    hw

---
+++ hw20.py
@@ -0,0 +1,10 @@
+import os
+
+dir_name = input("Полный путь к папке папки: ")
+objs = os.listdir(dir_name)
+for obj in objs:
+    p = os.path.join(dir_name, obj)
+    if os.path.isfile(p):
+        print(f'{obj} - file - {os.path.getsize(p)}')
+    elif os.path.isdir(p):
+        print(f'{obj} - dir')
