cd630541063d5632577b9bbe39db1182fdbf9997
JohnTenniel <johntenniel@outlook.com>
2024-07-06 10:50:16 +0700

    hw

--- two/dbsite.py
+++ two/dbsite.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, g
 import sqlite3
 import os
+from FDataBase import FDataBase
 
 DEBUG = True
 SECRET_KEY = 'a39e6c12f31952feb976c7ba631c2609d84b5da7'
@@ -24,8 +25,30 @@ def create_db():
     db.commit()
     db.close()
 
+
+def get_db():
+    if not hasattr(g, 'linl_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
 @app.route("/")
 def index():
     db = get_db()
-    return render_template('index.html')
+    dbase = FDataBase(db)
+    return render_template('index.html', menu=dbase.get_menu())
 
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_404.html', title="Страница не найдена", menu=[])
+
+
+if __name__ == '__main__':
+    app.run()
