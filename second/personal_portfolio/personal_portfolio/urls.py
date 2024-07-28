b00b46e2b082d8ae45189159ca42f8cba6a1cfca
JohnTenniel <johntenniel@outlook.com>
2024-07-28 08:29:10 +0700

    hw

--- second/personal_portfolio/personal_portfolio/urls.py
+++ second/personal_portfolio/personal_portfolio/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for password_generator_project project.
+URL configuration for personal_portfolio project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from generator import views
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from skills import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="home"),
-    path('password/', views.password, name="password"),
-    path('info/', views.info, name="info"),
+    path('', views.index, name='index'),
+    path('blog/', include('blog.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
