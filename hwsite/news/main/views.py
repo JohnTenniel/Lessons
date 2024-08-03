034c3b893a3efca6cef33575a6b66857dfedd86c
JohnTenniel <johntenniel@outlook.com>
2024-08-03 11:50:43 +0700

    hw

--- hwsite/news/main/views.py
+++ hwsite/news/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Main
 
+
 def index(request):
     projects = Main.objects.all()
     return render(request, 'main/index.html', {'projects': projects})
--- third/todos/todo/views.py
+++ third/todos/todo/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
 
 
 def signupuser(request):
@@ -9,10 +10,15 @@ def signupuser(request):
         return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-            user.save()
-            login(request, user)
-            return redirect('currenttodos')
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html',
+                              {'form': UserCreationForm(), 'error': 'Такое имя пользователя уже существует'
+                                                                    ', задайте другое'})
         else:
             return render(request, 'todo/signupuser.html',
                           {'form': UserCreationForm(), 'error': 'Пароли не совпадают'})
@@ -20,3 +26,17 @@ def signupuser(request):
 
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
+
+
+def home(request):
+    return render(request, 'todo/home.html')
+
+
+def logoutuser(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('home')
+
+def loginuser(request):
+    if request.method == "GET":
+        return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
